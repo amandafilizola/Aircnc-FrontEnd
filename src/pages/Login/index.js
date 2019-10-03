def7285b4a5d2a9393ba88ad47cbd85f14b095d7
Amanda Filizola <apf@cesar.org.br>
2019-10-03 15:31:44 -0300

    começando telas

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
 
-export default function Login() {
+export default function Login({ history }) {
     const [email, setEmail] = useState('');
 
     async function handleSubmit(event) {
@@ -10,17 +10,17 @@ export default function Login() {
       const { _id } = response.data;
   
       localStorage.setItem('user', _id);
-  
+      history.push('/dashboard');
     }
     return(
         <>
-            <p>Ofereça <strong>spots</strong> para progamadores e encontre <strong>talentos</strong> para sua empresa</p>
+          <p>Ofereça <strong>spots</strong> para progamadores e encontre <strong>talentos</strong> para sua empresa</p>
 
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">EMAIL*</label>
-          <input type="email" id="email" placeholder="seu melhor email" value={email} onChange={event =>setEmail(event.target.value)}/>
-          <button type="submit" className="btn">ENTRAR</button>
-        </form>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">EMAIL*</label>
+            <input type="email" id="email" placeholder="seu melhor email" value={email} onChange={event =>setEmail(event.target.value)}/>
+            <button type="submit" className="btn">ENTRAR</button>
+          </form>
         </>
     );
 }
